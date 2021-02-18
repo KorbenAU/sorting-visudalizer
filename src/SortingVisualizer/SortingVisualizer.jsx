de3da86b3baf72e5361ee8743397238131ebb77c
Korben Gao <yg@psychpress.com.au>
2021-02-18 15:05:09 +1100

    Add buttons in SV component

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -15,28 +15,41 @@ export default class SortingVisualizer extends Component {
     this.resetArray();
   }
 
-  resetArray() {
+  resetArray = () => {
     console.log('reset array');
     const array = [];
     for (let i = 0; i < 100; i++) {
       array.push(randomIntFromInterval(10, 800));
     }
-    console.log(array);
     this.setState({ array });
-  }
+  };
+
+  mergeSort = () => {};
+  quickSort = () => {};
+  heapSort = () => {};
+  bubbleSort = () => {};
 
   render() {
     const { array } = this.state;
     return (
-      <div className="array-container">
-        {array.map((value, index) => (
-          <div
-            className="array-bar"
-            key={index}
-            style={{ height: `${value}px` }}
-          ></div>
-        ))}
-      </div>
+      <>
+        <div className="array-container">
+          {array.map((value, index) => (
+            <div
+              className="array-bar"
+              key={index}
+              style={{ height: `${value}px` }}
+            ></div>
+          ))}
+        </div>
+        <div className="controller">
+          <button onClick={this.resetArray}>Generate New Array</button>
+          <button onClick={this.mergeSort}>Merge Sort</button>
+          <button onClick={this.quickSort}>Quick Sort</button>
+          <button onClick={this.heapSort}>Heap Sort</button>
+          <button onClick={this.bubbleSort}>Bubble Sort</button>
+        </div>
+      </>
     );
   }
 }
